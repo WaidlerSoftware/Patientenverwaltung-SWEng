9d9a7f18cf2d0909466b302d5bee8e1e5f185cce
Thurnbauer Matthias 3TRH <Matthias.Thurnbauer@rohde-schwarz.com>
2017-12-13 12:17:02 +0100

    Geschlecht hinzugefügt

--- sites/process.php
+++ sites/process.php
@@ -6,11 +6,12 @@
 // create a variable
 $vorname=$_POST['vorname'];
 $nachname=$_POST['nachname'];
+$geschlecht=$_POST['geschlecht'];
 $geburtsdatum=$_POST['geburtsdatum'];
 $email=$_POST['email'];
 $versicherung=$_POST['versicherung'];
  
 //Execute the query
  
-mysqli_query($connect,"INSERT INTO patient(vorname,nachname,geburtsdatum,email,versicherung)
-				VALUES('$vorname','$nachname','$geburtsdatum','$email','$versicherung')");
+mysqli_query($connect,"INSERT INTO patient(vorname,nachname,geschlecht,geburtsdatum,email,versicherung)
+				VALUES('$vorname','$nachname','$geschlecht','$geburtsdatum','$email','$versicherung')");
