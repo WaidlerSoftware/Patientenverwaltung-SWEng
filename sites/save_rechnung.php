ca828f2c51e42c97cdc12b11736bea01fc70f65f
Thurnbauer Matthias 3TRH <Matthias.Thurnbauer@rohde-schwarz.com>
2017-12-20 12:22:44 +0100

    geht wieder außer save_rechnung

--- sites/save_rechnung.php
+++ sites/save_rechnung.php
@@ -11,16 +11,12 @@ $gesamtbetrag=$_POST['gesamtbetrag'];
 $bankverbindung=$_POST['bankverbindung'];
  
 //Execute the query
- 
-$versicherung=$_POST['versicherung'];
-mysqli_query($connect,"INSERT INTO patient(anschrift,absender,erbrachte_Leistung,gesamtbetrag,bankverbindung)
+mysqli_query($connect,"INSERT INTO rechnung(anschrift,absender,erbrachte_Leistung,gesamtbetrag,bankverbindung)
 				VALUES('$anschrift','$absender','$erbrachte_Leistung','$gesamtbetrag','$bankverbindung')");
-
 if(mysqli_affected_rows($connect) > 0){
 	include("./rechnung_anlegen.html");	
 } else {
 	include("./rechnung_anlegen.html");
 	echo mysqli_error ($connect);
 }
-
 ?>
